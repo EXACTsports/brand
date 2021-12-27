88d19cea2a682e2cb3c801fd2fe87fec503ba88e
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2021-12-27 10:46:41 +0000

    Update table

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,20 +1,28 @@
 @php
-[$classes, $tableClass, $headClass, $additional] = match(config('brand.brand', 'exact')) {
-'exact' => ['min-w-full overflow-hidden overflow-x-auto align-middle shadow sm:rounded-lg', 'min-w-full divide-y divide-gray-200', 'bg-gray-100', 'exact'],
-'spect8' => ['align-middle min-w-full overflow-x-auto shadow overflow-hidden', 'min-w-full', 'bg-beige', 'spect8'],
+[$classes, $tableClass, $headClass, $footClass, $additional] = match(config('brand.brand', 'exact')) {
+'exact' => ['min-w-full overflow-hidden overflow-x-auto align-middle shadow sm:rounded-lg', 'min-w-full divide-y divide-gray-200', 'bg-gray-100', 'exact', ''],
+'spect8' => ['align-middle min-w-full overflow-x-auto shadow overflow-hidden', 'min-w-full', 'bg-beige', 'spect8', ''],
 };
 @endphp
 
 <div {{ $attributes->merge(['class' => $classes]) }} {{$additional}}>
-    <table class="{{$tableClass}}">
-        <thead class="{{$headClass}}">
+    <table {{ $attributes->merge(['class' => $tableClass]) }}>
+        <thead {{ $attributes->merge(['class' => $headClass]) }}>
             <tr>
                 {{ $head }}
             </tr>
         </thead>
 
         <tbody class="bg-white divide-y divide-cool-gray-200">
-            {{ $body }}
+            {{ $slot }}
         </tbody>
+        
+        @isset($foot)
+        <tfoot class="{{$footClass}}">
+            <tr>
+                {{ $foot ?? ''}}
+            </tr>
+        </tfoot>
+        @endisset
     </table>
 </div>
