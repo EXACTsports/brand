a8943526a46ba776df4ea113f85a63aa136d3eb8
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2022-01-19 23:51:43 -0600

    Tweak to naming convention

--- src/Components/ProgressSteps.php
+++ src/Components/ProgressSteps.php
@@ -2,9 +2,11 @@
 
 namespace App\View\Components;
 
+use Closure;
+use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class progressSteps extends Component
+class ProgressSteps extends Component
 {
 
     public $current;
@@ -24,7 +26,7 @@ class progressSteps extends Component
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     * @return View|Closure|string
      */
     public function render()
     {
