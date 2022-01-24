121c6733c723eacc32ac8d17a2930af63e384bd0
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2022-01-24 11:34:56 +0000

    Create tooltip.blade.php

---
+++ resources/views/components/tooltip.blade.php
@@ -0,0 +1,13 @@
+@props(['title' => ''])
+
+<div 
+    x-data="{ tooltip: false }" 
+    x-on:mouseover="tooltip = true" 
+    x-on:mouseleave="tooltip = false"
+    {{ $attributes->merge(['class' => 'cursor-pointer']) }}>
+    {{$title}}
+    <div x-show="tooltip"
+        class="absolute p-2 text-sm text-white transform translate-x-0 translate-y-0 bg-gray-800 rounded-lg">
+        {{$slot}}
+    </div>
+</div>
